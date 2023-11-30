f8fc200a64bafd6a4e0036f05a3a1b05fe67774f
Taras Priadka <taras@latch.bio>
2023-11-29 16:02:42 -0800

    Add clarifying comments

--- wf/__init__.py
+++ wf/__init__.py
@@ -22,12 +22,13 @@ metadata = LatchMetadata(
 @workflow(metadata)
 def automation_workflow(input_directory: LatchDir) -> None:
     output_directory = LatchOutputDir(
-        path="latch://FIXME"  # fixme: change to remote path of desired output directory
+        path="FIXME"  # fixme: change to latch:// path of desired output directory
     )
 
+    # See screenshots of where to find `target_wf_id`` and `table_id`` at this link <https://docs.latch.bio/automation/automation-usecase.html>
     automation_task(
         input_directory=input_directory,
         output_directory=output_directory,
-        target_wf_id="FIXME",  # fixme: change wf_id to the desired workflow id
+        target_wf_id="FIXME",  # fixme: change wf_id to the desired workflow id.
         table_id="FIXME",  # fixme: change table_id to the desired registry table
     )
